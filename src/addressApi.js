7cd49ea51ddd93fc9c0b7dfd2fc77d6fb77fbe58
Daniel <dselasea@gmail.com>
2023-03-19 18:05:22 +0000

    get ip address from api, add custom marker icon

---
+++ src/addressApi.js
@@ -0,0 +1,18 @@
+export const getInitialInfo = async () => {
+  const response = await fetch('https://api64.ipify.org/?format=json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify()
+  });
+
+  const data = await response.json();
+  getIpAddressInfo(data.ip);
+}
+
+export const getIpAddressInfo = async (info) => {
+  const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_pdfW24FBEWTWNqeCv7F4rtf1TNKbs&ipAddress=${info}`);
+  const data = await response.json();
+  return data;
+}
--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-const mapHeight = document.getElementById('map');
-mapHeight.style.height = '100vh';
-const map = L.map('map', {
-  center: [37.40599, -122.078514],
-  zoom: 13
-});
+import {getInitialInfo, getIpAddressInfo} from './addressApi.js';
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// Get element IDs
+const timeZone = document.getElementById('timeZone');
+const isp = document.getElementById('isp');
+const ipAddress = document.getElementById('ipAddress');
+
+console.log(getInitialInfo());
+
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+}
+
+function showPosition(position) {
+  createMap(position.coords.latitude, position.coords.longitude);
+}
+
+window.addEventListener('load', () => {
+  getLocation();
+})
+
+function createMap(lat, long) {
+  const mapHeight = document.getElementById('map');
+  mapHeight.style.height = '100vh';
+  const map = L.map('map', {
+    center: [lat, long],
+    zoom: 13
+  });
+  
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  let mapIcon = L.icon({
+    iconUrl: '/images/icon-location.svg',
+    iconSize: [46, 56],
+    iconAnchor: [22, 94],
+  });
+  
+  const  getMarkerPosition = L.marker([lat, long], {icon: mapIcon, draggable:true}).addTo(map);
+}
+
+
+
